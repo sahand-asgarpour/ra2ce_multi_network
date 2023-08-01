b59ad0ef85fa4a14b3adf7ad9d2ff34184370d73
sahand_asgarpour <sahand.asgarpour@deltares.nl>
2023-08-01 13:45:13 +0200

    chore: minor changes to the functions to generate meaningful gdf and removed jupyter notebook

--- ra2ce_multi_network/simplify_rail.py
+++ ra2ce_multi_network/simplify_rail.py
@@ -1,3 +1,5 @@
+from typing import Union
+
 import geopandas as gpd
 import numpy as np
 import pandas as pd
@@ -13,13 +15,14 @@ def detect_possible_terminals(network_gdf: gpd.GeoDataFrame, aggregation_range: 
     network = make_network_from_gdf(network_gdf=network_gdf)
     # hanging_nodes : An array of the indices of nodes with degree 1
     hanging_nodes = find_hanging_nodes(network=network)
-    possible_terminals = []
+    possible_terminal = []
     # Check for terminal criteria
-    possible_terminals = network.edges[['from_id', 'to_id', 'service']].apply(
+    possible_terminal = network.edges[['from_id', 'to_id', 'service']].apply(
         lambda x: check_terminal_criteria(x['from_id'], x['to_id'], x['service'], hanging_nodes), axis=1
     ).dropna().tolist()
-    network.nodes['possible_terminals'] = network.nodes['id'].apply(lambda x: 1 if x in possible_terminals else 0)
-    # Merge possible_terminals based on a aggregation_range
+    network.nodes['possible_terminal'] = network.nodes['id'].apply(lambda x: 1 if x in possible_terminal else 0)
+    # Merge possible_terminal based on an aggregation_range
+
     network.nodes = aggregate_terminal_nodes(network, aggregation_range=aggregation_range)
     return network
 
@@ -27,63 +30,82 @@ def detect_possible_terminals(network_gdf: gpd.GeoDataFrame, aggregation_range: 
 def aggregate_terminal_nodes(network: snkit.network.Network, aggregation_range: float) -> snkit.network.Network:
     # The following 2 functions are used in the body of merge_terminal_nodes function.
     # merge_terminal_nodes starts after the following 2 functions.
-    def update_node_gdf(node_gdf: gpd.GeoDataFrame, terminal_collection: gpd.GeoDataFrame, considered_node_ids: list)\
+    def update_node_gdf(node_gdf: gpd.GeoDataFrame, terminal_collection: gpd.GeoDataFrame, considered_node_ids: list) \
             -> gpd.GeoDataFrame:
-        terminal_collection_ids = terminal_collection['id'].to_numpy().tolist()
-        if len(terminal_collection_ids) == 1:
+        if len(terminal_collection['id'].to_numpy().tolist()) == 1:
             return node_gdf
+        if not any(node_id in considered_node_ids for node_id in terminal_collection['id'].to_numpy().tolist()):
+            aggregated_terminal_gdf = get_centroid_terminal_gdf(terminal_collection)
+            aggregated_ids = terminal_collection['id'].to_numpy().tolist()
         else:
+            # find terminal_to_aggr_again_gdf
+            terminal_to_aggr_again_gdf = node_gdf[
+                node_gdf['terminal_collection'].apply(
+                    lambda x: x is not None and any(
+                        node_id in x for node_id in terminal_collection['id'].to_numpy().tolist()) if isinstance(x, set)
+                    else False)
+            ]
+            aggregated_ids = terminal_to_aggr_again_gdf['id'].to_numpy().tolist()
 
-            if not any(node_id in considered_node_ids for node_id in terminal_collection_ids):
-                aggregated_terminal_gdf = get_centroid_terminal_gdf(terminal_collection)
-            else:
-                terminal_to_aggr_again_gdf = node_gdf[node_gdf['terminal_collection'].apply(
-                    lambda x: x is not None and any(node_id in x for node_id in terminal_collection_ids))]
-                aggregated_terminal_gdf = get_centroid_terminal_gdf(terminal_to_aggr_again_gdf)
-                aggregated_again = terminal_to_aggr_again_gdf[
-                    terminal_to_aggr_again_gdf['terminal_collection'].apply(lambda x: len(x) > 1)]['id'].tolist()
-                node_gdf = node_gdf[~node_gdf['id'].isin(aggregated_again)]
-            considered_node_ids += terminal_collection_ids
-            considered_node_ids = list(set(considered_node_ids))
-            return pd.concat([node_gdf, aggregated_terminal_gdf], ignore_index=True)
+            aggregated_terminal_gdf = get_centroid_terminal_gdf(terminal_to_aggr_again_gdf)
+            aggregated_again = terminal_to_aggr_again_gdf[
+                terminal_to_aggr_again_gdf['terminal_collection'].apply(lambda x: len(x) > 1)]['id'].tolist()
+            node_gdf = node_gdf[~node_gdf['id'].isin(aggregated_again)]
 
-    def get_centroid_terminal_gdf(_gdf: gpd.GeoDataFrame) -> gpd.GeoDataFrame:
+        node_gdf.loc[node_gdf['id'].isin(aggregated_ids), 'aggregated'] = 1
+        considered_node_ids += aggregated_ids
+        return pd.concat([node_gdf, aggregated_terminal_gdf], ignore_index=True)
+
+    def get_centroid_terminal_gdf(_gdf: gpd.GeoDataFrame) -> Union[gpd.GeoDataFrame, None]:
+        if _gdf.empty:
+            return None
         _centroid_terminal_collection = _gdf['geometry'].unary_union.centroid
         _terminal_collection_ids = _gdf['id'].tolist()
         _aggregated_terminal = Point(_centroid_terminal_collection.x, _centroid_terminal_collection.y)
 
         return gpd.GeoDataFrame({'id': new_id, 'geometry': _aggregated_terminal,
-                                 'possible_terminals': 1,
+                                 'possible_terminal': 1,
                                  'terminal_collection': [
                                      {term_col for _id in _terminal_collection_ids
                                       for term_col in node_gdf.loc[
                                           node_gdf['id'] == _id, 'terminal_collection'
                                       ].values[0]}
                                  ],
-                                 'buffer': _aggregated_terminal.buffer(aggregation_range)},
+                                 'aggregated': 0,
+                                 'buffer': _aggregated_terminal.buffer(aggregation_range),
+                                 },
                                 crs=node_gdf.crs)
 
     # merge_terminal_nodes function starts here
     considered_node_ids = []
     node_gdf = network.nodes
     node_gdf['terminal_collection'] = node_gdf.apply(
-        lambda row: {row['id']} if row['possible_terminals'] == 1 else None, axis=1)
+        lambda row: {row['id']} if row['possible_terminal'] == 1 else 'n.a.', axis=1)
+    node_gdf['aggregated'] = node_gdf.apply(
+        lambda row: 0 if row['possible_terminal'] == 1 else 'n.a.', axis=1)
+    if aggregation_range == 0:
+        return node_gdf
+
     node_gdf['buffer'] = node_gdf.apply(
-        lambda row: row.geometry.buffer(aggregation_range) if row['possible_terminals'] == 1 else None, axis=1)
+        lambda row: row.geometry.buffer(aggregation_range) if row['possible_terminal'] == 1 else None, axis=1)
     new_id = node_gdf['id'].max() + 1
 
-    for _, node in node_gdf[node_gdf["possible_terminals"] == 1].iterrows():
+    for _, node in node_gdf[node_gdf["possible_terminal"] == 1].iterrows():
         # Get possible_terminal nodes fall within the aggregation_range of each possible_terminal
         if node['id'] in considered_node_ids:
             continue
-        terminal_collection = node_gdf[node_gdf["possible_terminals"] == 1][
-            node_gdf[node_gdf["possible_terminals"] == 1].geometry.within(node['buffer'])]
-        # create the aggregated terminal based on the possible_terminals that it contains
+        terminal_collection = node_gdf[node_gdf["possible_terminal"] == 1][
+            node_gdf[node_gdf["possible_terminal"] == 1].geometry.within(node['buffer'])]
+        # create the aggregated terminal based on the possible_terminal that it contains
         node_gdf = update_node_gdf(node_gdf, terminal_collection, considered_node_ids)
         new_id += 1
 
     node_gdf.drop(columns='buffer', inplace=True)
-
+    node_gdf['terminal_collection'] = node_gdf['terminal_collection'].apply(
+        lambda val: sorted(list(val)) if isinstance(val, set) else val)
+    node_gdf['terminal_collection'] = node_gdf.apply(
+        lambda row: row['terminal_collection'] if isinstance(row['aggregated'], int) and row['aggregated'] == 0
+        else 'n.a.', axis=1)
     return node_gdf
 
 
