53e4cac9dae66f8c654c9400c63c75ba49df690a
sahand_asgarpour <sahand.asgarpour@deltares.nl>
2023-09-13 14:44:51 +0200

    chore: Notebook no.4 and relevant .py file is updated

--- ra2ce_multi_network/get_simplified_rail_network.py
+++ ra2ce_multi_network/get_simplified_rail_network.py
@@ -4,7 +4,7 @@ from ra2ce_multi_network.simplify_rail import _simplify_tracks
 
 ## Defining ini variables
 root_folder = OSM_DATA_DIR.parent
-study_area_suffix = '_test'
+study_area_suffix = '_ROTTERDAM_PORT'
 clip_output_name = f'study_area{study_area_suffix}'
 merged_network_file = root_folder.joinpath(f'networks/merged_rail_network_{clip_output_name}.geojson')
 
