eb1ea41494beafe9b5e38d5af355e933e389399e
sahand_asgarpour <sahand.asgarpour@deltares.nl>
2023-11-30 00:08:26 +0100

    feat: od mapped multi_modal are detected

--- ra2ce_multi_network/MultiModalGraph.py
+++ ra2ce_multi_network/MultiModalGraph.py
@@ -1,14 +1,9 @@
-# ToDo: add an attribute to od stating multi_modal_terminal
-# ToDo: rerun the od mapping for both merged rail and simplified road
 # ToDo: Define a function to detect the mapped multi_modal_ods in the separate graphs and connect the closest ones
-
+# ToDo: adjust the route finding algorithm
+import pandas as pd
 from pathlib import Path
-from typing import List, Dict, Tuple
-import networkx as nx
 import geopandas as gpd
 from networkx.classes.multigraph import MultiGraph
-from shapely.geometry import Point, MultiLineString
-from shapely.ops import linemerge
 from geopandas.geodataframe import GeoDataFrame
 
 
@@ -16,61 +11,58 @@ class MultiModalGraph:
     def __init__(
             self,
             od_file: Path,
-            graph_types: Dict[str, MultiGraph],
-            od_id_column: str = "id"
+            graph_types: dict[str, MultiGraph],
     ) -> None:
-        self.graph_types: Dict[str, MultiGraph] = graph_types
+        self.graph_types: dict[str, MultiGraph] = graph_types
         self.od_gdf: GeoDataFrame = gpd.read_file(od_file)
-        self.od_id_column: str = od_id_column
-        self._od_multi_modal_ods: Dict[str, Tuple[str, str]] = self._filter_ods()
+        self.od_multi_modal_ods: set = self._filter_ods()
 
         # Dynamically create attributes for mapped ods based on graph types
         for graph_type in graph_types:
-            setattr(self, f'{graph_type}_mapped_single_modal_ods', None)
-            setattr(self, f'{graph_type}_mapped_multi_modal_ods', None)
+            self._find_graph_mapped_ods(graph_type)
 
-    def _filter_ods(self, attr: str = 'multi_modal_terminal') -> Dict[str, Tuple[str, str]]:
-        filtered_ods: Dict[str, Tuple[str, str]] = {}
-        for index, row in self.od_gdf[self.od_gdf[attr] == 1].iterrows():
-            filtered_ods[row[self.od_id_column]] = (row['o_id'], row['d_id'])
-        return filtered_ods
+    def _filter_ods(self, attr: str = 'multi_modal_terminal', o_id_column: str = 'o_id', d_id_column: str = 'd_id') -> set:
+        _filtered_ods: set = set()
+        for _, row in self.od_gdf[self.od_gdf[attr] == 1].iterrows():
+            _filtered_ods.add(row[o_id_column])
+            _filtered_ods.add(row[d_id_column])
+        return _filtered_ods
 
     def _filter_mapped_ods(
             self,
             graph: MultiGraph,
-            mapped_ods: List[str],
-            is_multi_modal_ods: bool = True
-    ) -> Dict[str, str]:
+            mapped_ods: list[str],
+            is_multi_modal_ods: bool = True,
+            od_id_column: str = "od_id"
+    ) -> dict[str, str]:
         return {
-            node: data['od_id'] for node, data in graph.nodes(data=True)
-            if node in mapped_ods and
-               any(
-                   f'O_{i}' in data['od_id'].split(',') or
-                   f'D_{i}' in data['od_id'].split(',')
-                   for i in range(1, len(self.od_gdf) + 1)
-                   if f'O_{i}' in data['od_id'] or f'D_{i}' in data['od_id']
-               ) and (node in self._od_multi_modal_ods.values()) == is_multi_modal_ods
+            node: data[od_id_column] for node, data in graph.nodes(data=True)
+            if node in mapped_ods and any(
+                (element in self.od_multi_modal_ods) == is_multi_modal_ods
+                for element in data.get('od_id', "").split(',')
+            )
         }
 
-    def find_graph_mapped_ods(self, graph_type: str) -> None:
+    def _find_graph_mapped_ods(self, graph_type: str, od_id_column: str = "od_id") -> None:
         # Ensure the provided graph type is valid
         if graph_type not in self.graph_types:
             raise ValueError(f"Invalid graph type: {graph_type}")
 
         # Find nodes in the specified graph type with 'O_number' or 'D_number' in 'od_id' attribute
-        od_graph_mapped_nodes: List[str] = [
+        od_graph_mapped_nodes: list[str] = [
             node for node, data in self.graph_types[graph_type].nodes(data=True)
-            if any(
-                f'O_{i}' in data.get('od_id', '').split(',') or
-                f'D_{i}' in data.get('od_id', '').split(',')
+            if not pd.isna(data.get(od_id_column, '')) and any(
+                f'O_{i}' in data.get(od_id_column, '').split(',') or
+                f'D_{i}' in data.get(od_id_column, '').split(',')
                 for i in range(0, len(self.od_gdf) + 1)
-                if f'O_{i}' in data.get('od_id', '') or f'D_{i}' in data.get('od_id', '')
+                if f'O_{i}' in data.get(od_id_column, '') or f'D_{i}' in data.get(od_id_column, '')
             )
         ]
 
         # Filter mapped ods for the specified graph type
         mapped_multi_modal_ods, mapped_single_modal_ods = (
-            self._filter_mapped_ods(self.graph_types[graph_type], od_graph_mapped_nodes, True)
+            self._filter_mapped_ods(self.graph_types[graph_type], od_graph_mapped_nodes, True),
+            self._filter_mapped_ods(self.graph_types[graph_type], od_graph_mapped_nodes, False)
         )
 
         # Store the results based on the specified graph type
---
+++ ra2ce_multi_network/multi_modal_graph_test.py
@@ -0,0 +1,36 @@
+# Example usage
+from pathlib import Path
+import pickle
+from ra2ce_multi_network.MultiModalGraph import MultiModalGraph
+import networkx as nx
+from snkit import Network
+from ra2ce_multi_network.simplify_rail import _nx_to_network, _network_to_nx
+
+root_folder = Path(r'C:\Users\asgarpou\OneDrive - Stichting Deltares\Documents\Projects\Long involvement\SITO_Netwerk to system\project_files\multi_network')
+od_file_path = root_folder.joinpath('static/network/od_nodes.geojson')
+
+# Create a MultiGraph for testing (replace this with your actual graph creation logic)
+with open(root_folder.joinpath(f'static/output_graph/road_simple_graph_od_mapped_study_area_GR.p'), 'rb') as road_f:
+    road_graph = pickle.load(road_f)
+
+with open(root_folder.joinpath(f'static/output_graph/merged_rail_network_od_mapped_study_area_GR.geojson'), 'rb') as rail_f:
+    rail_network = pickle.load(rail_f)
+
+rail_graph = _network_to_nx(rail_network)
+
+# Define graph types
+graph_types = {'road': road_graph, 'rail': rail_graph}
+
+# Create an instance of MultiModalGraph
+multi_modal_graph = MultiModalGraph(od_file_path, graph_types)
+
+# Access the results for the 'road' graph type
+print("Mapped Multi-Modal ODs for Road Graph:", multi_modal_graph.road_mapped_multi_modal_ods)
+print("Mapped Single-Modal ODs for Road Graph:", multi_modal_graph.road_mapped_single_modal_ods)
+
+# Test for the 'rail' graph type
+multi_modal_graph._find_graph_mapped_ods('rail')
+
+# Access the results for the 'rail' graph type
+print("Mapped Multi-Modal ODs for Rail Graph:", multi_modal_graph.rail_mapped_multi_modal_ods)
+print("Mapped Single-Modal ODs for Rail Graph:", multi_modal_graph.rail_mapped_single_modal_ods)
