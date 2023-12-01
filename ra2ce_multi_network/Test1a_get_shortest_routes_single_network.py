6f7239830090ccc7411f86ce215baebc4a12dbc2
sahand_asgarpour <sahand.asgarpour@deltares.nl>
2023-12-02 00:08:36 +0100

    chore: Test1a is changed to adapt with the latest ra2ce architecture

--- ra2ce_multi_network/Test1a_get_shortest_routes_single_network.py
+++ ra2ce_multi_network/Test1a_get_shortest_routes_single_network.py
@@ -5,18 +5,22 @@ import pickle
 from osm_flex.config import *
 import networkx as nx
 
-from ra2ce.analyses.analysis_config_data.readers.analysis_config_reader_factory import AnalysisConfigReaderFactory
+from ra2ce.analyses.analysis_config_data.analysis_config_data_reader import AnalysisConfigDataReader
 from ra2ce.analyses.indirect.analyses_indirect import IndirectAnalyses
+from ra2ce.graph.graph_files.graph_files_collection import GraphFilesCollection
 from ra2ce.graph.network_config_data.network_config_data import NetworkConfigData
 from ra2ce_multi_network.simplify_rail import _network_to_nx
 
 # Defining ini variables
 root_folder = Path(
-    r'C:\Users\asgarpou\OneDrive - Stichting Deltares\Documents\Projects\Long involvement\SITO_Netwerk to system\project_files\get_shortest_routes')
+    # r'C:\Users\asgarpou\OneDrive - Stichting Deltares\Documents\Projects\SITO_Netwerk to system\content\input_data\pbf'
+    r'C:\Users\asgarpou\osm'
+)
 ## Defining ini variables
-study_area_suffix = '_GR'  # small case study area that works: '_ROTTERDAM_PORT'
+study_area_suffix = '_GR' # small case study area that works: '_ROTTERDAM_PORT'
 clip_output_name = f'study_area{study_area_suffix}'
-rail_net_file = root_folder.joinpath(f'static/network/merged_rail_network_{clip_output_name}.geojson')
+rail_net_file = root_folder.joinpath(f'networks/merged_rail_network_{clip_output_name}.geojson')
+analysis_folder = root_folder.joinpath('analysis/Test1a_optimal_rotes_rail')
 
 # Load rail_net
 with open(rail_net_file, 'rb') as handle:
@@ -49,12 +53,12 @@ od_gdf['category'] = 'terminal'
 od_gdf['flow'] = [i * 10 for i in range(1, len(od_gdf) + 1)]
 od_gdf = od_gdf.rename(columns={"id": "OBJECTID"})
 
-od_gdf.to_feather(root_folder.joinpath(f'static/output_graph/origin_destination_table.feather'))
+od_gdf.to_feather(analysis_folder.joinpath(f'static/output_graph/origin_destination_table.feather'))
 
 # convert rail network to a NetworkX graph
 graph = _network_to_nx(rail_net)
 
-# Setting up the traffic analysis based on ra2ce
+# Setting up the network and analysis config based on ra2ce
 network_config = NetworkConfigData()
 network_config.network.primary_file = graph
 network_config.output_path = root_folder.joinpath(f'output')
@@ -62,16 +66,22 @@ network_config.static_path = root_folder.joinpath(f'static')
 network_config.project.name = 'rail'
 network_config_dict = vars(network_config)
 
-analysis_ini = root_folder.joinpath(r'analysis.ini')
+analyses_ini = analysis_folder.joinpath(r'analysis.ini')
+analyses_config = AnalysisConfigDataReader().read(ini_file=analyses_ini)
+analyses = IndirectAnalyses(config=analyses_config, graph_files=GraphFilesCollection())
+analyses.graph_files.origins_destinations_graph = graph
 
-analysis_config = AnalysisConfigReaderFactory().read(ini_file=analysis_ini, network_config=network_config_dict)
-analysis = IndirectAnalyses(config=analysis_config, graphs=graph)
+analyses = IndirectAnalyses(config=analyses_config, graph_files=analyses.graph_files)
 
-# Run
-optimal_routes_gdf = analysis.optimal_route_origin_destination(graph, analysis.config['indirect'][0])
-optimal_routes_gdf = optimal_routes_gdf[~optimal_routes_gdf['geometry'].is_empty]
+for analysis in analyses_config.analyses:
+    func = getattr(analyses, analysis.analysis.config_value)
+    result = func(graph, analysis)
+    result = result[~result['geometry'].is_empty]
 
-# Save
-optimal_routes_gdf.to_pickle(root_folder / 'output' / f'optimal_routes{study_area_suffix}.pkl')
-optimal_routes_gdf.to_csv(path_or_buf=root_folder / 'output' / f'optimal_routes{study_area_suffix}.csv', index=False)
-a = 1
+    # Save
+    result.to_pickle(root_folder / 'analysis' / 'Test1a_optimal_rotes_rail' / 'output' /
+                     f'{analysis.analysis.config_value}_{study_area_suffix}.pkl')
+    result.to_csv(path_or_buf=root_folder / 'analysis' / 'Test1a_optimal_rotes_rail' / 'output' /
+                              f'{analysis.analysis.config_value}_{study_area_suffix}.csv',
+                  index=False)
+
