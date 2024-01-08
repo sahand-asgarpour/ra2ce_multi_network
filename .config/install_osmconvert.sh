50de14b86baa0289bd7eea456068308e020ed6cf
sahand_asgarpour <sahand.asgarpour@deltares.nl>
2024-01-08 16:59:27 +0100

    fix: .config cleaned

--- .config/install_osmconvert.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-curl -O http://m.m.i24.cc/osmconvert.c
-gcc -x c osmconvert.c -o osmconvert -lz -O3
-rm osmconvert.c
-
-# wget -O - http://m.m.i24.cc/osmconvert.c | gcc -x c - -lz -O3 -o osmconvert
-# curl -O - http://m.m.i24.cc/osmconvert.c | gcc -x c - -lz -O3 -o osmconvert
